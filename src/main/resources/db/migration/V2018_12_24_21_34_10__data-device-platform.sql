aca68045b653a451ebca0740cce8b8455d2309d8
dimmaryanto93 <software.dimas_m93@icloud.com>
2018-12-24 21:40:21 +0700

    update example data

---
+++ src/main/resources/db/migration/V2018_12_24_21_34_10__data-device-platform.sql
@@ -0,0 +1,3 @@
+insert into device_category(id, name, description)
+values ('001', 'Komputer', 'Alat electronik seperti PC, dan Laptop'),
+       ('002', 'Telekomunikasi', 'Alat telekomunikasi seperti HP, Telphone dll');
